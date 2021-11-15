1c3d10f9f28e170f1d56c1071288536d4654126c
JazzPants <leighton.l14@gmail.com>
2021-11-16 10:41:28 +1100

    Done Second Commit

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ class Deciduous extends Tree {
     }//this.name more dynamic than just writing "Deciduous"
 }
 
+//this actually violates substition principle, use definitionTree() in a grandparent class
+
+console.log(Deciduous);
+
 class Evergreen extends Tree {
     constructor(species, name) {
         super(species);
@@ -28,3 +32,55 @@ class Evergreen extends Tree {
             return `${super.definition()} Evergreens keep their leaves all year round.`
     }
 }
+
+
+/*
+
+// Write your classes here
+class Tree {
+    constructor(species) {
+        this.species = species;
+    }
+
+    static definitionTree() {
+        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+    }
+}
+
+class Deciduous extends Tree {
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
+    static definition() { //new function that overrides original, don't need super.definition
+            return `${definitionTree()} ${this.name} trees shed their leaves annually.`
+    }//this.name more dynamic than just writing "Deciduous"
+}
+
+//this actually violates substition principle, use definitionTree() in a grandparent class
+
+console.log(Deciduous);
+
+class Evergreen extends Tree {
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
+    static definition() { //new function that overrides original, don't need super.definition
+            return `${definitionTree()} Evergreens keep their leaves all year round.`
+    }
+}
+
+*/
+
+/*
+function User(name, email) {
+    this.name = name;
+    this.email = email;
+}
+
+let lauren = new User("Lauren", "lauren@example.com");
+lauren.name //=> "Lauren"
+*/
+
+//lauren is the new context, so we can use this.name, and this.email inside lauren object!
